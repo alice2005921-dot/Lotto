b8971bc3c170f6da06cb809d00d143f5ca8825a2
li <alice2005921@gamil.com>
2025-09-15 16:55:23 +0800

    第五次commit (水平垂直置中)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
@@ -34,16 +37,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Play( modifier: Modifier = Modifier) {
     var lucky = (1..100).random()
+    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(
+            text = "樂透數字(1-100)為 $lucky",
+        )
 
-    Text(
-        text = "樂透數字(1-100)為 $lucky",
-        modifier = modifier
-    )
+        Button(
+            onClick = { lucky = (1..100).random() }
+        ) {
+            Text("重新產生樂透碼")
 
-    Button(
-        onClick = { lucky = (1..100).random() }
-    ) {
-        Text("重新產生樂透碼")
-
+        }
     }
 }
+
